a08630b02ea93a186866bc2b94e3388fc7e834ab
Dane Barentine <dane.barentine@software.dell.com>
2016-07-26 10:48:35 -0700

    Only set admin role desc/scope on create. Otherwise it will force full entity update which can cause threading issues

--- services/src/main/java/org/keycloak/services/managers/RealmManager.java
+++ services/src/main/java/org/keycloak/services/managers/RealmManager.java
@@ -290,6 +290,8 @@ public class RealmManager implements RealmImporter {
             adminRealm = realm;
 
             adminRole = realm.addRole(AdminRoles.ADMIN);
+            adminRole.setDescription("${role_"+AdminRoles.ADMIN+"}");
+            adminRole.setScopeParamRequired(false);
 
             RoleModel createRealmRole = realm.addRole(AdminRoles.CREATE_REALM);
             adminRole.addCompositeRole(createRealmRole);
@@ -299,8 +301,6 @@ public class RealmManager implements RealmImporter {
             adminRealm = model.getRealm(Config.getAdminRealm());
             adminRole = adminRealm.getRole(AdminRoles.ADMIN);
         }
-        adminRole.setDescription("${role_"+AdminRoles.ADMIN+"}");
-        adminRole.setScopeParamRequired(false);
 
         ClientModel realmAdminApp = KeycloakModelUtils.createClient(adminRealm, KeycloakModelUtils.getMasterRealmAdminApplicationClientId(realm.getName()));
         // No localized name for now
